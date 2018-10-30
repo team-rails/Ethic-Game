240c5ec61a8b3ab7b0bb65c12bb82bd286471d83
EC2 Default User <ec2-user@ip-172-31-16-241.us-east-2.compute.internal>
2018-10-30 01:50:10 +0000

    Basic scenarios page

--- ethic_game/app/controllers/welcome_controller.rb
+++ ethic_game/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @scenarioNames = ["Scenario 1", "Scenario 2", "Scenario 3"];
+  end
+  
+  def show
+    puts "show called"
   end
 end
