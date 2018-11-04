5f3f69573bba676efeb486ec71f55ebdead8bf0c
EC2 Default User <ec2-user@ip-172-31-16-241.us-east-2.compute.internal>
2018-11-04 15:45:56 +0000

    Added a scenario detail page

--- ethic_game/app/controllers/welcome_controller.rb
+++ ethic_game/app/controllers/welcome_controller.rb
@@ -3,6 +3,10 @@ class WelcomeController < ApplicationController
     @scenarioNames = ["Test Scenario", "Water Boiling Exercise", "Team Building Exercise"];
   end
   
+  def detail
+    @scenario = params[:scenario]
+  end
+  
   def show
     puts "show called"
   end
--- ethic_game/config/routes.rb
+++ ethic_game/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get '/:scenario' => 'welcome#show', as: 'show'
+  get '/show/:scenario' => 'welcome#show', as: 'show'
+  get '/:scenario' => 'welcome#detail', as: 'detail'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
