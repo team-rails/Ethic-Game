bf3b5c5d3a14e62aa1a260eedbb96090ec6f9865
EC2 Default User <ec2-user@ip-172-31-17-63.us-east-2.compute.internal>
2018-11-14 03:05:59 +0000

    Input and group tabs

--- ethic_game/config/routes.rb
+++ ethic_game/config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # get '/:scenario' => 'welcome#show', as: 'show'
   #get '/show/:scenario' => 'welcome#show', as: 'show'
   get 'welcome/:id' => 'welcome#detail', as: 'detail'
-  get 'show/:id' => 'welcome#show', as: 'show'
-  get 'show_history/:id' => 'welcome#show_history', as: 'show_history'
+  get 'show/:id(/:active)' => 'welcome#show', as: 'show'
+  get 'show_history/:id(/:active)' => 'welcome#show_history', as: 'show_history'
+  post 'show/:id(/:active)' => 'welcome#show', as: 'show_input'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
