a6738ceecf7819d95860377151aea0671de4cccd
EC2 Default User <ec2-user@ip-172-31-16-241.us-east-2.compute.internal>
2018-11-14 03:59:45 +0000

    Added get all responses for a group

--- ethic_game/app/controllers/welcome_controller.rb
+++ ethic_game/app/controllers/welcome_controller.rb
@@ -27,10 +27,33 @@ class WelcomeController < ApplicationController
     @question_history = PossibleQuestion.get_question_by_history(@histories)
     @response_history = PossibleResponse.get_response_by_history(@histories)
     
+    #Test code for getQuestionResponses
+    groups = Group.all
+    group = groups[0]
+    group_possible_questions = GroupsPossibleQuestion.where("group_id = ?", group.id)
+    group_possible_question = group_possible_questions[0];
+    #puts("Group possible questions = #{group_possible_question}")
+    responses = getQuestionResponses(group_possible_question)
+    responses.each do |resp|
+      puts("Response is #{resp.response}")
+    end
   end
   
   def show_history
     
+  end
+  
+  def getQuestionResponses(group_question) 
+    # Send in group possible questions
+    responses = []
+    question_responses = PossibleQuestionsResponse.where("possible_question_id = ?", group_question.possible_question_id)
+    question_responses.each do |question_response|
+      #Loop through all the questions responses objects and gets there possible response
+      
+      resp = question_response.possible_response
+      responses.push(resp)
+    end
+    return responses
   end
   
 end
