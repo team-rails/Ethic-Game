d9b822dbb6f33b7b57b07616416f08080f760e14
EC2 Default User <ec2-user@ip-172-31-16-241.us-east-2.compute.internal>
2018-12-02 21:45:14 +0000

    Ready to merge to master

--- ethic_game/config/routes.rb
+++ ethic_game/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/index', as: 'index'
+  get 'welcome/index'
   get 'welcome/:id' => 'welcome#detail', as: 'detail'
   post 'welcome/create_player/:scenario_id' => 'welcome#create_player', as: 'create_player'
   get 'show_history/:id(/:active_id)' => 'welcome#show_history', as: 'show_history'
