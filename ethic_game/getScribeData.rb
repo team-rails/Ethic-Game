ed225708de38d81df05971748d0d682c7d2d8e87
EC2 Default User <ec2-user@ip-172-31-29-141.us-east-2.compute.internal>
2019-04-19 21:54:48 +0000

    Updated Ruby script

--- ethic_game/getScribeData.rb
+++ ethic_game/getScribeData.rb
@@ -10,7 +10,7 @@ end
 for files in scribe_file_paths
 	QuestionArray = Array.new()
 	ResponseArray = Array.new()
-	open("SampleScribe.txt", "r").each_line do |line|
+	open(files, "r").each_line do |line|
 		data = line.split('=>')	
 		#print data[0].gsub(/ +/, " "), data[1].gsub(/ +/, " ")
 		QuestionArray.push(data[0].strip)
