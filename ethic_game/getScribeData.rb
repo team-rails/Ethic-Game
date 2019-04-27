507930bd56df0e94e807576ca40e1ebd11738be4
EC2 Default User <ec2-user@ip-172-31-29-141.us-east-2.compute.internal>
2019-04-27 15:57:41 +0000

    Python Interface v1

--- ethic_game/getScribeData.rb
+++ ethic_game/getScribeData.rb
@@ -10,11 +10,26 @@ end
 for files in scribe_file_paths
 	QuestionArray = Array.new()
 	ResponseArray = Array.new()
+	line_index = 0
 	open(files, "r").each_line do |line|
-		data = line.split('=>')	
+		data		= line.split('=>')	
+		#line_index	= caller.data
 		#print data[0].gsub(/ +/, " "), data[1].gsub(/ +/, " ")
-		QuestionArray.push(data[0].strip)
-		ResponseArray.push(data[1].strip)
+		QuesArray_temp = Array.new()
+		RespArray_temp = Array.new()
+		
+		QuesArray_temp.push(data[0].strip)
+		QuesArray_temp.push('0')			#Numerical Value for Reponse
+		QuesArray_temp.push(line_index)
+
+		RespArray_temp.push(data[1].strip)
+		RespArray_temp.push('0')			
+		RespArray_temp.push('0')
+		
+		QuestionArray.insert(line_index, QuesArray_temp)
+		ResponseArray.insert(line_index, RespArray_temp)
+		
+		line_index += 1
 	end
 	print QuestionArray
 	print ResponseArray
